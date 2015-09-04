2baf06cff4bb456776c50d18b24b242618667397
anthrgrnwrld <anthrgrnwrld@gmail.com>
2015-09-04 19:39:27 +0900

    commit

--- PanTest/ViewController.swift
+++ PanTest/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    var inButton = false
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.label.text = "\(count)"
+        let myTap = UIPanGestureRecognizer(target: self, action: "didPan:")
+        self.button.addGestureRecognizer(myTap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pressButton(sender: AnyObject) {
+        count++
+        self.label.text = "\(count)"
     }
+    
+    func didPan(sender: AnyObject) {
+        println("a")
 
+        if let pan = sender as? UIPanGestureRecognizer {
+
+            let p = pan.locationInView(self.view)
+            if !inButton &&  self.button.frame.contains(p)
+            {
+                count++
+                inButton = !inButton
+                self.label.text = "\(count)"
+            }else if inButton && !self.button.frame.contains(p) {
+                inButton = !inButton
+            }
+        }
+    }
 
 }
 
